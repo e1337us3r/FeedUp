1e31ef4076b9902183a4c90c6e4c7953543f55ac
Sinan Sakaoglu <sinanssakaoglu@gmail.com>
2018-08-12 01:09:41 +0300

    Fix port settings.

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(5000, params => {
+app.listen(PORT, params => {
   console.log("Server listening on port " + PORT);
 });
